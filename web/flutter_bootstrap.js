4194e7189b8303ac57ebf8fe1e594edfa12d962e
seonghyerim <tjdgpfla@gmail.com>
2026-05-26 10:45:42 +0900

    deploy: 2026-05-26 10:45

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2219361465" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2296353642" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
